8de1d6393c69a88034e2834633ee51186f24c5a6
InsBonin <luizhenriquewbonin@gmail.com>
2024-10-22 21:12:30 -0300

    Front Intro

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,17 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - Meu Site</title>
-    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kalnia:wght@100..700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../css/app.css">
+
 </head>
 <body>
     
     @include('partials.navbar')
 
-    <div class="container mt-5">
+    <div class="container text-center mt-5">
         @yield('content')
     </div>
 
     @include('partials.footer')
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,9 +4,96 @@
 
 @section('content')
 
-<div>
-    <h2>Postagens</h2>
-    <p>Todas as postagens</p>
+<div style="font-family: Kalnia;">
+
+    <!-- Título do Blog -->
+    <h1 class="display-3">NOME DO BLOG</h1>
+    <p class="lead">Frase de impacto</p>
+    
+    <!-- Cards (3 blocos lado a lado) -->
+    <div class="row my-5">
+        <div class="col-md-4">
+            <div class="card p-4">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-8 text-start">
+                            <h5 class="card-title mb-0">Lorem Ipsum</h5>
+                            <p class="card-text text-muted">Ipsum dummy</p>
+                        </div>
+                        <div class="col-4">
+                            <span class="border rounded-circle p-4 d-block"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card p-4">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-8 text-start">
+                            <h5 class="card-title mb-0">Lorem Ipsum</h5>
+                            <p class="card-text text-muted">Ipsum dummy</p>
+                        </div>
+                        <div class="col-4">
+                            <span class="border rounded-circle p-4 d-block"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card p-4">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-8 text-start">
+                            <h5 class="card-title mb-0">Lorem Ipsum</h5>
+                            <p class="card-text text-muted">Ipsum dummy</p>
+                        </div>
+                        <div class="col-4">
+                            <span class="border rounded-circle p-4 d-block"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Seção de ícones circulares (4 ícones) -->
+    <div class="d-flex justify-content-center my-4">
+      <div class="p-2"><span class="border rounded-circle p-4 d-block"></span></div>
+      <div class="p-2"><span class="border rounded-circle p-4 d-block"></span></div>
+      <div class="p-2"><span class="border rounded-circle p-4 d-block"></span></div>
+      <div class="p-2"><span class="border rounded-circle p-4 d-block"></span></div>
+    </div>
+
+    <!-- Seção de artigos (Lista de posts) -->
+    <div class="row my-5">
+      <div class="col-md-8">
+        <div class="mb-4">
+          <h5>Título do Artigo Que Será Destaque</h5>
+          <small class="text-muted">Categoria - Data</small>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia erat vel dapibus tincidunt.</p>
+        </div>
+        <div class="mb-4">
+          <h5>Título Do Artigo Que Será Destaque</h5>
+          <small class="text-muted">Categoria - Data</small>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia erat vel dapibus tincidunt.</p>
+        </div>
+      </div>
+
+      <!-- Seção lateral (lista de links) -->
+      <div class="col-md-4">
+        <h5>Talvez você goste</h5>
+        <ul class="list-unstyled">
+          <li><a href="#">Título do Artigo 1</a></li>
+          <li><a href="#">Título do Artigo 2</a></li>
+          <li><a href="#">Título do Artigo 3</a></li>
+        </ul>
+      </div>
+    </div>
+
+
     <div>
         @foreach($postagens as $postagem)
         <div>
