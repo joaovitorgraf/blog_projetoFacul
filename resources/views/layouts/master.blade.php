50e683dc14fa9969ebadc53a34528375eb91fd25
InsBonin <54009731+InsBonin@users.noreply.github.com>
2024-10-24 20:59:29 -0300

    Melhorando o index

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,18 +1,26 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - Meu Site</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- Fonte Kalnia -->
     <link href="https://fonts.googleapis.com/css2?family=Kalnia:wght@100..700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Fonte Inter -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <!-- CSS -->
     <link rel="stylesheet" href="../../css/app.css">
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/9ec46122f2.js" crossorigin="anonymous"></script>
 
 </head>
+
 <body>
-    
+
     @include('partials.navbar')
 
     <div class="container text-center mt-5">
@@ -21,8 +29,9 @@
 
     @include('partials.footer')
 
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
+
 </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -70,7 +70,7 @@
     <!-- Seção de artigos (Lista de posts) -->
     <div class="row my-5">
         <div class="col-md-8">
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <ul class="nav nav-tabs" id="myTab" style="font-family: Inter;" role="tablist">
                 <li class="nav-item" role="presentation">
                     <button class="nav-link active" id="top-tab" data-bs-toggle="tab" data-bs-target="#top-tab-pane" type="button" role="tab" aria-controls="top-tab-pane" aria-selected="true">TOP ARTIGOS</button>
                 </li>
@@ -99,11 +99,27 @@
                         </div>
                         <div class="row mt-3">
                             <div class="col-md-8">
-                                <h3>Titulo do artigo ficará aqui</h3>
-                                <p>Aqui haverá um descritivo inicial sobre o artigo</p>
+                                <div class="row" style="text-align: start; margin-left: 2%;">
+                                    <div class="col-12 m-0 p-0">
+                                        <h3>Titulo do artigo ficará aqui</h3>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-4 m-0 p-0">
+                                        <h5 style="font-size: 0.8rem;">01 setembro 2024</h5>
+                                    </div>
+                                    <div class="col-6 m-0 p-0 d-flex flex-row justify-content-center">
+                                        <h5 style="font-size: 0.8rem;"><i class="fa-regular fa-heart"></i> 12k</h5>
+                                        <h5 style="font-size: 0.8rem; margin-left: 5%;"><i class="fa-regular fa-comments"></i> 100k</h5>
+                                    </div>
+                                    <div class="col-2 m-0 p-0">
+                                        <h5 style="font-size: 0.8rem;"><i class="fa-solid fa-share"></i></h5>
+                                    </div>
+                                </div>
                             </div>
                             <div class="col-md-4">
-                                <p>IMAGEM</p>
+                                <img style="width: 100%;" src="https://images.unsplash.com/photo-1444653614773-995cb1ef9efa?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
                             </div>
                         </div>
                     </div>
@@ -113,35 +129,70 @@
             <div class="tab-pane fade" id="lazer-tab-pane" role="tabpanel" aria-labelledby="lazer-tab" tabindex="0">Lazer</div>
             <div class="tab-pane fade" id="entretenimento-tab-pane" role="tabpanel" aria-labelledby="entretenimento-tab" tabindex="0">Entretenimento</div>
         </div>
-    </div>
 
-    <!-- Seção lateral (lista de links) -->
-    <div class="col-md-4">
-        <h5>Talvez você goste</h5>
-        <ul class="list-unstyled">
-            <li><a href="#">Título do Artigo 1</a></li>
-            <li><a href="#">Título do Artigo 2</a></li>
-            <li><a href="#">Título do Artigo 3</a></li>
-        </ul>
-    </div>
-</div>
-
-
-<div>
-    @foreach($postagens as $postagem)
-    <div>
-        <img src="/img/capa/{{ $postagem->capa }}" alt="{{ $postagem->titulo }}">
-        <div>
-            <p>Data da Postagem: {{ $postagem->data_postagem }}</p>
-            <h5>Titulo: {{ $postagem->titulo }}</h5>
-            <p>Conteudo: {{ $postagem->conteudo }}</p>
-            <p>Autor: {{ $usuario[$postagem->id_usuario]->name ?? 'Desconhecido' }}</p>
-
-            <a href="/postagem/{{ $postagem->id }}">Ver mais</a>
+        <!-- Seção lateral (lista de links) -->
+        <div class="col-md-4">
+            <h5 class="ms-4 d-flex justify-content-start mt-5 mb-4" style="font-family: Inter; font-weight: bold; font-size: 1rem;">Talvez você goste</h5>
+            <ul class="list-unstyled ms-4">
+                <li>
+                    <div class="row mt-3 justify-content-center align-items-center">
+                        <div class="col-sm-2 m-0 p-0">
+                            <span class="border rounded-circle p-4 d-block"></span>
+                        </div>
+                        <div class="col-sm-4 ms-2 m-0 p-0" style="align-items: start;">
+                            <a style="font-size: 0.9rem;">Nome Colunista</a>
+                        </div>
+                        <div class="col-sm-4 m-0 p-0 d-flex justify-content-end">
+                            <input class="btn btn-secondary btn-sm" type="button" value="ver mais" style="font-size: 0.8rem;">
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="row mt-3 justify-content-center align-items-center">
+                        <div class="col-sm-2 m-0 p-0">
+                            <span class="border rounded-circle p-4 d-block"></span>
+                        </div>
+                        <div class="col-sm-4 ms-2 m-0 p-0" style="align-items: start;">
+                            <a style="font-size: 90%;">Nome Colunista</a>
+                        </div>
+                        <div class="col-sm-4 m-0 p-0 d-flex justify-content-end">
+                            <input class="btn btn-secondary btn-sm" type="button" value="ver mais" style="font-size: 0.8rem;">
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="row mt-3 justify-content-center align-items-center">
+                        <div class="col-sm-2 m-0 p-0">
+                            <span class="border rounded-circle p-4 d-block"></span>
+                        </div>
+                        <div class="col-sm-4 ms-2 m-0 p-0" style="align-items: start;">
+                            <a style="font-size: 90%;">Nome Colunista</a>
+                        </div>
+                        <div class="col-sm-4 m-0 p-0 d-flex justify-content-end">
+                            <input class="btn btn-secondary btn-sm" type="button" value="ver mais" style="font-size: 0.8rem;">
+                        </div>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
-    @endforeach
-</div>
+
+
+    <div>
+        @foreach($postagens as $postagem)
+        <div>
+            <img src="/img/capa/{{ $postagem->capa }}" alt="{{ $postagem->titulo }}">
+            <div>
+                <p>Data da Postagem: {{ $postagem->data_postagem }}</p>
+                <h5>Titulo: {{ $postagem->titulo }}</h5>
+                <p>Conteudo: {{ $postagem->conteudo }}</p>
+                <p>Autor: {{ $usuario[$postagem->id_usuario]->name ?? 'Desconhecido' }}</p>
+
+                <a href="/postagem/{{ $postagem->id }}">Ver mais</a>
+            </div>
+        </div>
+        @endforeach
+    </div>
 </div>
 
 @endsection
